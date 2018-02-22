0c355fa69a2d011628c6b0de898c047ede66539c
webworker01 <webworker@live.com>
2018-02-22 12:30:19 -0500

    Remove password protect

--- memcache.php
+++ memcache.php
@@ -32,19 +32,19 @@ $MEMCACHE_SERVERS[] = 'localhost:11211'; // add more as an array
 ////////// END OF DEFAULT CONFIG AREA /////////////////////////////////////////////////////////////
 
 ///////////////// Password protect ////////////////////////////////////////////////////////////////
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
-           $_SERVER['PHP_AUTH_USER'] != ADMIN_USERNAME ||$_SERVER['PHP_AUTH_PW'] != ADMIN_PASSWORD) {
-			Header("WWW-Authenticate: Basic realm=\"Memcache Login\"");
-			Header("HTTP/1.0 401 Unauthorized");
+// if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ||
+//            $_SERVER['PHP_AUTH_USER'] != ADMIN_USERNAME ||$_SERVER['PHP_AUTH_PW'] != ADMIN_PASSWORD) {
+// 			Header("WWW-Authenticate: Basic realm=\"Memcache Login\"");
+// 			Header("HTTP/1.0 401 Unauthorized");
 
-			echo <<<EOB
-				<html><body>
-				<h1>Rejected!</h1>
-				<big>Wrong Username or Password!</big>
-				</body></html>
-EOB;
-			exit;
-}
+// 			echo <<<EOB
+// 				<html><body>
+// 				<h1>Rejected!</h1>
+// 				<big>Wrong Username or Password!</big>
+// 				</body></html>
+// EOB;
+// 			exit;
+// }
 
 ///////////MEMCACHE FUNCTIONS /////////////////////////////////////////////////////////////////////
 
